fc767c5bb68f0f87860f74a2232f2074ac1e3eb2
Vishalra-i <vishalrai0392@gmail.com>
2024-03-19 10:57:54 +0530

    first code in Next Js

---
+++ src/app/chai/lemon/page.tsx
@@ -0,0 +1,8 @@
+export default function lemon(){
+    return(
+        <div>
+            <h1>lemon</h1>
+        </div>
+    )
+
+}
---
+++ src/app/chai/page.tsx
@@ -0,0 +1,3 @@
+export default function chaiPage(){
+     return  <h3 className="mt-8 text-3xl  text-center "> Chai Page </h3>
+}
