98a96993f2a8f7a48c9e583d936da28cd632c3d4
Vishalra-i <vishalrai0392@gmail.com>
2024-04-15 16:09:35 +0530

    Added acertinity ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+import FeaturedSection from "@/components/FeaturedSection";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructor from "@/components/Instructor";
+import TestiminoalCard from "@/components/TestiminoalCard";
+import Upcoming from "@/components/Upcoming";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-          <h1 className="text-3xl text-center">Hello World</h1>
+    <main className="min-h-screen  bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+          <HeroSection/>
+          <FeaturedSection/>
+          <WhyChooseUs/>
+          <TestiminoalCard/>
+          <Upcoming/>
+          <Instructor/>
+          <Footer/>
     </main>
   );
 }
