b97045dd569490cae80707c27b2ce569d22027fe
Vishal Rai <135126334+Vishalra-i@users.noreply.github.com>
2026-02-27 11:38:42 +0530

    Update homepage for tiles products and inquiry location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,13 @@
 import FeaturedSection from "@/components/FeaturedSection";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
-import Instructor from "@/components/Instructor";
-import TestiminoalCard from "@/components/TestiminoalCard";
-import Upcoming from "@/components/Upcoming";
-import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
-    <main className="min-h-screen  bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-          <HeroSection/>
-          <FeaturedSection/>
-          <WhyChooseUs/>
-          <TestiminoalCard/>
-          <Upcoming/>
-          <Instructor/>
-          <Footer/>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedSection />
+      <Footer />
     </main>
   );
 }
